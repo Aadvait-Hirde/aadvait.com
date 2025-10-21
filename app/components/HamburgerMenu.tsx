9ee7eb98c27cc0f419867ff507a88e38f40a183e
Aadvait Hirde <ahirde@iu.edu>
2025-10-21 18:39:46 -0400

    1.1 git commit for hamburger menu

--- app/components/HamburgerMenu.tsx
+++ app/components/HamburgerMenu.tsx
@@ -6,14 +6,15 @@ interface HamburgerMenuProps {
 
 export default function HamburgerMenu({ onClick }: HamburgerMenuProps) {
   return (
-    <div className="fixed top-8 right-8 md:top-12 md:right-12 z-40 hidden lg:block">
+    <div className="fixed top-8 right-8 md:top-12 md:right-12 z-40">
       <button 
         onClick={onClick}
         className="w-12 h-12 flex flex-col items-center justify-center gap-1.5 hover:opacity-70 transition-opacity"
       >
-        <span className="w-8 h-0.5 bg-orange-500" />
-        <span className="w-8 h-0.5 bg-orange-500" />
-        <span className="w-8 h-0.5 bg-orange-500" />
+        {/* Black bars on mobile (orange bg), orange bars on desktop (black bg) */}
+        <span className="w-8 h-0.5 bg-black lg:bg-orange-500" />
+        <span className="w-8 h-0.5 bg-black lg:bg-orange-500" />
+        <span className="w-8 h-0.5 bg-black lg:bg-orange-500" />
       </button>
     </div>
   );
